a0dcd65bec53f5a483a7480202dfeaefeea311f9
CamiloFetecua <camilofetecua2011@gmail.com>
2024-01-06 13:54:50 -0800

    update author file

---
+++ header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#include <signal.h>
+
+
+
+
+
+
+#endif
