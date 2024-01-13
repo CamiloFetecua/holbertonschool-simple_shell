730805d4588bb3904249bf74a9ffcaef5873126e
CamiloFetecua <camilofetecua2011@gmail.com>
2024-01-13 10:13:46 -0800

    fix bug in path

--- checker.c
+++ checker.c
@@ -1,108 +1,20 @@
 #include "header.h"
-#include <stddef.h>
-#include <stdlib.h>
-
 /**
- * _realloc - Entry point
- * Description: 'to reallocate memory'
- *
- * @ptr: pointer
- * @size: size
- * Return: 'the new ptr'
+ * error_message - prints out error message
+ * @tokens: array containing user input
+ * @argv: command line arguments array
+ * @exit_status: exit status
+ * Return: void
  */
-void *_realloc(void *ptr, size_t size)
+void error_message(char **tokens, char **argv, int *exit_status)
 {
-	void *new_ptr;
-	char *next_ptr;
-	char *new_next_ptr;
-	size_t dup_size;
-	size_t index = 0; /* counter */
+	char newline = '\n';
 
-	if (size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	} /* end if */
-	/* we allocate a new block of memory */
-	new_ptr = malloc(size);
-	/* validate allocation  */
-	if (new_ptr == NULL)
-	{
-		return (NULL);
-	} /* end if */
-	/* copy contents to new blocks */
-	next_ptr = (char *) ptr;
-	new_next_ptr = (char *) next_ptr;
-	if (size < sizeof(ptr))
-	{
-		dup_size = size;
-	} /* end if */
-	else
-	{
-		dup_size = sizeof(ptr);
-	} /* end else */
-
-	for (; index <= dup_size; index++)
-	{
-		new_next_ptr[index] = next_ptr[index];
-	} /* end for */
-
-	free(ptr);
-
-	return (next_ptr);
-
-} /* end function */
-
-
-/**
- * _getline - Entry point
- * Description: 'to read a line of char.'
- *
- * @linePtr: 'pointer to the line'
- * @buff_size: 'buffer'
- * @my_file: 'the file'
- * Return: raed line
- */
-
-ssize_t _getline(char **linePtr, size_t *buff_size, FILE *my_file)
-{
-	ssize_t bytes_read = 0;
-	size_t index = 0;
-	ssize_t result;
-	char *newSizePtr;
-
-	while (1)
-	{
-		if (index >= *buff_size)
-		{
-			*buff_size += 8;
-			newSizePtr = _realloc(*linePtr, *buff_size);
-			if (newSizePtr == NULL)
-			{
-				perror("Memory allocation error");
-				return (-1);
-			} /* End if */
-			*linePtr = newSizePtr;
-		} /* end if */
-		result = read(fileno(my_file), &((*linePtr)[index]), 1);
-		if (result == -1)
-		{
-			perror("Error reading file");
-			return (-1);
-		} /* end if */
-		else if (result == 0)
-		{
-			break;
-		} /* end else if */
-		bytes_read++;
-
-		if ((*linePtr)[index] == '\n')
-		{
-			break;
-		} /* end if */
-
-		index++;
-	} /* end while */
-
-	return (bytes_read);
-} /* end function */
+	write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+	write(STDERR_FILENO, ": 1: ", 5);
+	write(STDERR_FILENO, tokens[0], _strlen(tokens[0]));
+	write(STDERR_FILENO, ": Illegal number: ", 18);
+	write(STDERR_FILENO, tokens[1], _strlen(tokens[1]));
+	write(STDERR_FILENO, &newline, 1);
+	*exit_status = 2;
+}
